3ed39708aff0122545278d54f424019d24394855
open-the-world <43783380+open-the-world@users.noreply.github.com>
2020-03-11 22:57:37 +0900

    fix url of category

--- routes/web.php
+++ routes/web.php
@@ -24,22 +24,22 @@ Route::get('/ranking', 'RankingController@index');
 
 Route::get('/category', 'CategoryController@index');
 
-Route::get('/category/programming', 'category\Category_programmingController@index');
-Route::get('/category/design', 'category\Category_designController@index');
-Route::get('/category/language', 'category\Category_languageController@index');
-Route::get('/category/sports', 'category\Category_sportsController@index');
-Route::get('/category/study', 'category\Category_studyController@index');
+Route::get('/category/programming', 'Category\Category_programmingController@index');
+Route::get('/category/design', 'Category\Category_designController@index');
+Route::get('/category/language', 'Category\Category_languageController@index');
+Route::get('/category/sports', 'Category\Category_sportsController@index');
+Route::get('/category/study', 'Category\Category_studyController@index');
 Route::get('/category/work', 'category\Category_workController@index');
-Route::get('/category/health', 'category\Category_healthController@index');
-Route::get('/category/finance', 'category\Category_financeController@index');
-Route::get('/category/happiness', 'category\Category_happinessController@index');
-Route::get('/category/thinking', 'category\Category_thinkingController@index');
-Route::get('/category/music', 'category\Category_musicController@index');
-Route::get('/category/cooking', 'category\Category_cookingController@index');
-Route::get('/category/video', 'category\Category_videoController@index');
-Route::get('/category/daily', 'category\Category_dailyController@index');
-Route::get('/category/skill', 'category\Category_skillController@index');
-Route::get('/category/educate', 'category\Category_educateController@index');
+Route::get('/category/health', 'Category\Category_healthController@index');
+Route::get('/category/finance', 'Category\Category_financeController@index');
+Route::get('/category/happiness', 'Category\Category_happinessController@index');
+Route::get('/category/thinking', 'Category\Category_thinkingController@index');
+Route::get('/category/music', 'Category\Category_musicController@index');
+Route::get('/category/cooking', 'Category\Category_cookingController@index');
+Route::get('/category/video', 'Category\Category_videoController@index');
+Route::get('/category/daily', 'Category\Category_dailyController@index');
+Route::get('/category/skill', 'Category\Category_skillController@index');
+Route::get('/category/educate', 'Category\Category_educateController@index');
 
 Route::group(['middleware' => 'auth'], function() {
   Route::get('/mypage', 'MypageController@index');
