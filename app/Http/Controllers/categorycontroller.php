17d15e6cb54a32031bf3c8d38e543e62a89a660f
open-the-world <43783380+open-the-world@users.noreply.github.com>
2020-03-11 22:03:53 +0900

    fix name of controller

--- app/Http/Controllers/categorycontroller.php
+++ app/Http/Controllers/categorycontroller.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Post;
 use Storage;
 
-class Categorycontroller extends Controller
+class CategoryController extends Controller
 {
   public function index()
   {
